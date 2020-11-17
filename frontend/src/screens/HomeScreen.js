a7b4bfe0895a1548601eaf521ea75d39a1c6121c
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-11-17 15:02:54 +0530

    added bootstrap and layout components

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import products from '../products';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+    </>
+  );
+};
+
+export default HomeScreen;
