e0ac3481926be524f226dc35cdc254e60232405f
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-11-19 20:47:56 +0530

    fetching data frm db and serving in frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -13,13 +14,6 @@ app.get('/', (req, res) => {
   res.send('running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, console.log(`server started in ${process.env.NODE_ENV} on ${PORT}`));
