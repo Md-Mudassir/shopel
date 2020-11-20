b57df650d197055c391cb5168c94066e33be8a8a
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-11-21 00:45:35 +0530

    added redux with actions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -15,5 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`server started in ${process.env.NODE_ENV} on ${PORT}`));
